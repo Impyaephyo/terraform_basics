c4d66f013dfcb9b15f3471f97b34d0b0b2d98af7
Pyae  Phyo <pyaephyo@Pyaes-MacBook-Pro.local>
2024-02-10 13:32:41 +0800

    added diagram

--- local.tf
+++ local.tf
@@ -1,4 +1,3 @@
 # In the terraform not only can use Cloud providers but also local file as well.
 # Those are the code to create local file in the terraform.
 
-/Users/pyaephyo/Desktop/Screen Shot 2024-02-10 at 13.28.38 PM.png
